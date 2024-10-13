dd16e53fceecc176acdd1706f2ce088ab04bcf2b
ikosafo <ikosafo@gmail.com>
2024-10-13 13:22:52 +0000

    Payment Options

---
+++ paymentOptions.php
@@ -0,0 +1,104 @@
+<?php include ('./includes/header.php'); ?>
+
+    <!-- Main -->
+    <main>
+
+        <!-- Title -->
+        <section class="hero" data-aos="fade">
+            <div class="hero-bg">
+                <img src="<?php echo URLROOT ?>assets/img/dwl/01.jpg" srcset="<?php echo URLROOT ?>assets/img/dwl/01.jpg 2x" alt="">
+            </div>
+            <div class="bg-content container">
+                <div class="hero-page-title">
+                    <span class="hero-sub-title">Tours & Packages</span>
+                    <h1 class="display-3 hero-title">
+                        Payment Options
+                    </h1>
+                </div>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb mb-0">
+                        <li class="breadcrumb-item"><a href="/">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Payment Options</li>
+                    </ol>
+                </nav>
+            </div>
+        </section>
+        <!-- /Title -->
+
+
+        <!-- About -->
+        <section class="p-top-90 p-bottom-90 bg-gray-gradient" data-aos="fade">
+            <div class="container">
+                <div class="row g-0">
+                <div class="col-12 col-xl-6 order-1 order-xl-0">
+                        <!-- Image -->
+                        <div class="pe-xl-5">
+                            <div class="image-info image-info-right image-info-vertical">
+                                <div class="image-center">
+                                    <img src="<?php echo URLROOT ?>assets/img/dwl/payment.jpg" srcset="<?php echo URLROOT ?>assets/img/dwl/payment.jpg 2x" class="rounded w-100" alt="">
+                                    <img src="<?php echo URLROOT ?>assets/img/dwl/payment2.jpg" srcset="<?php echo URLROOT ?>assets/img/dwl/payment2.jpg 2x" class="rounded w-100 mt-3" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /Image -->
+                    </div>
+                    <div class="col-12 col-xl-6 order-0 order-xl-1">
+                        <!-- Content -->
+                        <div class="pt-xl-4 mb-xl-0 mb-5">
+                            <div class="block-title">
+                                <small class="sub-title">Tours & Packages</small>
+                                <h2 class="h1 title">Payment Options</h2>
+                            </div>
+                            <p>
+                                At Nhyiraba Travel & Tour Services, we strive to make the payment process simple and secure. We offer
+                                 a variety of payment options to ensure our clients have the flexibility and convenience they 
+                                 need when booking their travels or services. Below are the available payment methods:
+                            </p>
+                            <div class="mnw-200">
+                               <h3 class="h5">Why Book With Us?</h3>
+                            </div>
+                            <ul class="highlight-list checked">
+                                <li>
+                                    <span><strong>Credit/Debit Card Payments:</strong> <br>We accept all major credit and debit cards, including: Visa, MasterCard, American Express and Discover</span>
+                                </li>
+                                <li>
+                                    <span><strong>Mobile Money:</strong> <br>For clients in Ghana and other supported regions, we offer Mobile Money payment options, which allow you to pay directly from your mobile device. You can use the following services: MTN Mobile Money, AirtelTigo Money and Telecel Cash</span>
+                                </li>
+                                <li>
+                                    <span><strong>Bank Transfer:</strong> <br>We also accept payments via direct bank transfer.</span>
+                                </li>
+                                <li>
+                                    <span><strong>PayPal:</strong> <br>For international clients, we offer PayPal as a secure and reliable payment method. You can use your PayPal balance, linked bank account, or credit/debit card to complete the payment. This option allows for quick and easy transactions, no matter where you are in the world.</span>
+                                </li>
+                                <li>
+                                    <span><strong>Cash Payments (In-Person):</strong> <br>If you prefer to make a payment in person, you can visit our office to make a cash payment. Our friendly staff will assist you with completing your booking and issuing a receipt for your payment.</span>
+                                </li>
+                                <li>
+                                    <span><strong>Payment Plans:</strong> <br>We understand that travel can be a significant investment. To make it more manageable, we offer flexible payment plans for select services. This option allows you to spread the cost over multiple payments, ensuring that you can secure your booking while budgeting effectively.</span>
+                                </li>
+                            </ul>
+                        </div>
+
+                            <div class="pt-3">
+                                <a href="contact" class="btn btn-primary mnw-180">
+                                    <i class="hicon hicon-email-envelope"></i>
+                                    <span>Let's talk now</span>
+                                </a>
+                                <a href="tel:+233303955227" class="btn btn-link link-dark link-hover fw-medium mnw-180">
+                                    <i class="hicon hicon-telephone"></i>
+                                    <span>030-3955227</span>
+                                </a>
+                            </div>
+                        </div>
+                        <!-- /Content -->
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- /About -->
+
+
+    </main>
+    <!-- /Main -->
+
+<?php include ('./includes/footer.php'); ?>
